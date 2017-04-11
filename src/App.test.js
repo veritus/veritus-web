99954ad3ddb015d460f866776f3ee48e0f8821dc
= <=>
2017-04-11 19:19:32 +0200

    Adding Flow

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
+/* @flow */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+describe('App base file', () => {
+
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
 });
