7890c3389120a47eafc0ca443169be17634aff5f
Ari Hrannar Björnsson <arihrannar@gmail.com>
2017-05-23 19:09:09 +0200

    case list now renders

---
+++ src/Containers/CaseList/index.js
@@ -0,0 +1,28 @@
+/* @flow */
+import React from 'react';
+import CaseCard from '../../Components/CaseCard';
+export class Case extends React.Component {
+
+  render() {
+    const cases = [
+      {
+        id: 1,
+        name: 'Case1',
+      },
+      {
+        id: 2,
+        name: 'Case2',
+      },
+      {
+        id: 3,
+        name: 'Case3',
+      },
+    ];
+    return (
+      <div>
+        {cases.map(parliament_case => <CaseCard parliament_case={parliament_case} />)}
+      </div>
+    );
+  }
+}
+export default Case;
