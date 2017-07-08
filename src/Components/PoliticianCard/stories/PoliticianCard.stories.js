32bb509e9c641ebcbd7c848d4f68dc3bbbe5410e
ragnar.hardarson <ragnarhardarson90@gmail.com>
2017-07-08 17:01:04 +0200

    Dead code

--- src/Components/PoliticianCard/stories/PoliticianCard.stories.js
+++ src/Components/PoliticianCard/stories/PoliticianCard.stories.js
@@ -3,21 +3,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { muiTheme } from 'storybook-addon-material-ui';
 import PoliticianCard from '../index';
-import type { PromiseType } from '../../../types';
 import { politicians } from '../../../utils/testFixtures';
-
-const promises: Array<PromiseType> = [
-  {
-    id: 1,
-    title: 'Longer workdays',
-    fulfilled: false,
-  },
-  {
-    id: 2,
-    title: '3 day weekends',
-    fulfilled: true,
-  },
-];
 
 storiesOf('Politican Card', module)
   .addDecorator(muiTheme())
