a1257d842db576dc79114cd403a1f91574df6f75
ragnar <ragnarhardarson90@gmail.com>
2017-07-11 19:11:34 +0200

    Subjects can be picked in dropdown

--- src/Components/SubjectPicker/index.js
+++ src/Components/SubjectPicker/index.js
@@ -1,8 +1,48 @@
 /* @flow */
-import React from 'react';
+import React, { Component } from 'react';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
+import type { SubjectType } from '../../types';
 
-const SubjectPicker = () => {
-  return <div>Im a subject picker</div>;
+export type Props = {
+  subjects: Array<SubjectType>,
 };
+
+class SubjectPicker extends Component {
+  props: Props;
+
+  state = {
+    values: [],
+  };
+
+  handleChange = (event: SyntheticEvent, index: number, values: Array<number>) =>
+    this.setState({ values });
+
+  menuItems(values: Array<number>) {
+    return this.props.subjects.map((subject: SubjectType) =>
+      <MenuItem
+        key={subject.id}
+        insetChildren={true}
+        checked={values && values.indexOf(subject.id) > -1}
+        value={subject.id}
+        primaryText={subject.name}
+      />
+    );
+  }
+
+  render() {
+    const { values } = this.state;
+    return (
+      <SelectField
+        multiple={true}
+        hintText="Select a name"
+        value={values}
+        onChange={this.handleChange}
+      >
+        {this.menuItems(values)}
+      </SelectField>
+    );
+  }
+}
 
 export default SubjectPicker;
--- src/Components/SubjectPicker/stories/SubjectPicker.stories.js
+++ src/Components/SubjectPicker/stories/SubjectPicker.stories.js
@@ -4,8 +4,9 @@ import { storiesOf } from '@storybook/react';
 import centered from '@storybook/addon-centered';
 import { muiTheme } from 'storybook-addon-material-ui';
 import SubjectPicker from '../index';
+import { subjects } from '../../../utils/testFixtures';
 
 storiesOf('Subject Picker', module)
   .addDecorator(centered)
   .addDecorator(muiTheme())
-  .add('Default', () => <SubjectPicker />);
+  .add('Default', () => <SubjectPicker subjects={subjects} />);
