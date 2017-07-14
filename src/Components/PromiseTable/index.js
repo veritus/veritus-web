e2a4a2c88ba0cdaf1e724ef52eb6739e382fdeab
ragnar <ragnarhardarson90@gmail.com>
2017-07-14 22:03:25 +0200

    Super simple table for promises

---
+++ src/Components/PromiseTable/index.js
@@ -0,0 +1,46 @@
+/* @flow */
+import React from 'react';
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn,
+} from 'material-ui/Table';
+import type { PromiseType } from '../../types';
+
+export type Props = {
+  promises: Array<PromiseType>,
+};
+
+const PromiseTable = (props: Props) => {
+  const { promises } = props;
+  if (!promises) {
+    return null;
+  }
+  const promiseRows = promises.map(promise =>
+    <TableRow key={promise.id}>
+      <TableRowColumn>{promise.name}</TableRowColumn>
+      <TableRowColumn>{promise.politician}</TableRowColumn>
+      <TableRowColumn>{promise.small_description}</TableRowColumn>
+    </TableRow>
+  );
+
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHeaderColumn>Name</TableHeaderColumn>
+          <TableHeaderColumn>Politician id</TableHeaderColumn>
+          <TableHeaderColumn>Short description</TableHeaderColumn>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {promiseRows}lel
+      </TableBody>
+    </Table>
+  );
+};
+
+export default PromiseTable;
