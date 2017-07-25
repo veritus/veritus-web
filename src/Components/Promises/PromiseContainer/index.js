1f28fb0f412c34abb4bdcda22359938e59eab3f5
ragnar <ragnarhardarson90@gmail.com>
2017-07-25 21:50:21 +0200

    placeholder promises

---
+++ src/Components/Promises/PromiseContainer/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+export class PromiseContainer extends React.Component {
+  state = {
+    promises: [],
+  };
+
+  render() {
+    return (
+      <div>
+        hello
+      </div>
+    );
+  }
+}
+
+export default PromiseContainer;
