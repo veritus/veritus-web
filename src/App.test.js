179295e7e9a47aa00402d68f3ee543292b56c9cf
ragnar <ragnarhardarson90@gmail.com>
2017-07-30 16:47:59 +0200

    Root route is Politicians. So serve those

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,13 @@
 /* @flow */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import fetchMock from 'fetch-mock';
+
 import App from './App';
+import { politicians } from './utils/testFixtures';
+
+// Root route is Politicians. So serve those
+fetchMock.get('end:/api/v1/politicians/', politicians);
 
 describe('App base file', () => {
   it('renders without crashing', () => {
