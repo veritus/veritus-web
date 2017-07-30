2992868967d4439c1d64fd2ff7b05af9957148e8
ragnar <ragnarhardarson90@gmail.com>
2017-07-30 19:22:56 +0200

    Reselect for cleaner state <=> data

--- src/Stores/rootReducer.js
+++ src/Stores/rootReducer.js
@@ -1,8 +1,10 @@
 /* @flow */
 import { combineReducers } from 'redux';
+import PoliticiansReducer from './Politicians/reducer';
 import promisesReducer from './Promises/reducer';
 
 const reducer = combineReducers({
+  politicians: PoliticiansReducer,
   promises: promisesReducer,
 });
 
