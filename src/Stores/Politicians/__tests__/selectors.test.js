c6ac78956d564813de0073c5cc75ee713d501f89
ragnar <ragnarhardarson90@gmail.com>
2017-08-08 22:19:00 +0200

    Reduxing politicians

--- src/Stores/Politicians/__tests__/selectors.test.js
+++ src/Stores/Politicians/__tests__/selectors.test.js
@@ -1,11 +1,26 @@
 /* @flow */
 
-import { politiciansSelector } from '../selectors';
+import { politiciansSelector, politiciansLoaded } from '../selectors';
 import { initialState, politicians } from '../../../utils/testFixtures';
 
 describe('Politicians selector', () => {
   it('loads initial state', () => {
     expect(politiciansSelector(initialState)).toEqual(null);
+  });
+
+  it('knows politicans have not been loaded', () => {
+    expect(politiciansLoaded(initialState)).toEqual(false);
+  });
+
+  it('knows politicians have tried to load', () => {
+    const loadedEmptyState = {
+      ...initialState,
+      politicians: {
+        ...initialState.politicians,
+        politicians: [],
+      },
+    };
+    expect(politiciansLoaded(loadedEmptyState)).toEqual(true);
   });
 
   it('Returns politicians', () => {
