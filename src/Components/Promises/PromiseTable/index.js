aec81ba584fb3e7169c5963c39261ebba8e8722c
Ragnar Hardarson <ragnarhardarson90@gmail.com>
2017-08-15 17:58:09 +0200

    Promise table (#47)
    * Super simple table for promises
    
    * More promises for realistic table view
    
    * placeholder header for promise actions
    
    * Rendering fulfillment status
    
    * Moving into new file-structure

---
+++ src/Components/Promises/PromiseTable/index.js
@@ -0,0 +1,60 @@
+/* @flow */
+import React from 'react';
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn,
+} from 'material-ui/Table';
+import InCompleteIcon from 'material-ui/svg-icons/navigation/close';
+import CompleteIcon from 'material-ui/svg-icons/navigation/check';
+import { red500, green500 } from 'material-ui/styles/colors';
+import type { PromiseType } from '../../../types';
+
+export type Props = {
+  promises: Array<PromiseType>,
+};
+
+const getIcon = fulfilled =>
+  fulfilled ? <CompleteIcon color={green500} /> : <InCompleteIcon color={red500} />;
+
+const PromiseTable = (props: Props) => {
+  const { promises } = props;
+  if (!promises) {
+    return null;
+  }
+
+  const promiseRows = promises.map(promise =>
+    <TableRow key={promise.id}>
+      <TableRowColumn>{promise.name}</TableRowColumn>
+      <TableRowColumn>{promise.politician}</TableRowColumn>
+      <TableRowColumn>{promise.small_description}</TableRowColumn>
+      <TableRowColumn>{getIcon(promise.fulfilled)}</TableRowColumn>
+    </TableRow>
+  );
+
+  return (
+    <Table fixedHeader={true} multiSelectable={true}>
+      <TableHeader>
+        <TableRow>
+          <TableHeaderColumn colSpan="3">
+            Here be buttons
+          </TableHeaderColumn>
+        </TableRow>
+        <TableRow>
+          <TableHeaderColumn>Name</TableHeaderColumn>
+          <TableHeaderColumn>Politician id</TableHeaderColumn>
+          <TableHeaderColumn>Short description</TableHeaderColumn>
+          <TableHeaderColumn>Fulfilled</TableHeaderColumn>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {promiseRows}lel
+      </TableBody>
+    </Table>
+  );
+};
+
+export default PromiseTable;
