b3f8e2216500838de9ed4d6be8ecbec6a7151b6d
ragnar.hardarson <ragnarhardarson90@gmail.com>
2017-11-11 21:15:21 +0100

    Relative address

--- src/config.js
+++ src/config.js
@@ -1,4 +1,2 @@
 export const serverBaseUrl =
-  process.env.NODE_ENV === 'production'
-    ? 'http://http://163.172.132.165/api'
-    : 'http://localhost:8000/api';
+  process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost/api';
