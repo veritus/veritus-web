0947a351e6d933660f146fb5419bc33e5cb6c889
ragnar.hardarson <ragnarhardarson90@gmail.com>
2017-11-14 20:27:46 +0100

    Map subjects to promises

--- src/Stores/Promises/__tests__/selectors.test.js
+++ src/Stores/Promises/__tests__/selectors.test.js
@@ -1,78 +1,16 @@
 /* @flow */
 
-import { promisesSelector } from '../selectors';
+import deepFreeze from 'deep-freeze';
+import { promisesSelector, promiseSubjectsSelector } from '../selectors';
 import { initialState, politicians, promises } from '../../../utils/testFixtures';
+import type { Subject } from '../../../types';
 
 describe('Promises selector', () => {
   it('loads initial state', () => {
     expect(promisesSelector(initialState)).toEqual(null);
   });
 
-  it('Returns null if politicians arent loaded', () => {
-    const loadedState = {
-      ...initialState,
-      promises: {
-        ...initialState.promises,
-        promises: [promises[0]],
-      },
-    };
-    expect(promisesSelector(loadedState)).toEqual(null);
-  });
-
   it('Returns promises', () => {
-    // TODO use the enriched politician object
-    // const politician = {
-    //   name: 'Andrés Ingi Jónsson',
-    //   id: 1,
-    //   initials: 'AIJ',
-    //   districtNumber: 10,
-    //   party: {
-    //     name: 'Vinstri Hreyfingin - Grænt Framboð',
-    //     id: 3,
-    //     website: 'http://vg.is/',
-    //     created: '2017-04-25T17:41:28Z',
-    //     modified: '2017-04-25T17:41:28Z',
-    //   },
-    //   district: {
-    //     name: 'Reykjavik Norður’',
-    //     id: 1,
-    //     abbreviation: 'Reykv. N.',
-    //     created: '2017-04-25T17:41:28Z',
-    //     modified: '2017-04-25T17:41:28Z',
-    //   },
-    //   promises: [
-    //     {
-    //       name: 'Some title',
-    //       small_description: 'Another title',
-    //       long_description: 'Yet another title',
-    //       parliament: {
-    //         name: 'Test-Parliament',
-    //         id: 1,
-    //       },
-    //       id: 4,
-    //       created: '2017-06-06T19:36:10.404869Z',
-    //       modified: '2017-06-06T19:36:10.404898Z',
-    //       politician: 2,
-    //       party: 1,
-    //       fulfilled: false,
-    //     },
-    //     {
-    //       name: 'Raise taxes',
-    //       small_description: 'Short description',
-    //       long_description: 'long\ndescription\nexplaining the entire thing',
-    //       parliament: {
-    //         name: 'Test-Parliament',
-    //         id: 1,
-    //       },
-    //       id: 5,
-    //       created: '2017-06-06T19:38:33.526074Z',
-    //       modified: '2017-06-06T19:38:33.526105Z',
-    //       politician: 2,
-    //       party: 1,
-    //       fulfilled: false,
-    //     },
-    //   ],
-    // };
     const politician = 2;
     const combinedPromise = {
       name: 'Some title',
@@ -102,7 +40,90 @@ describe('Promises selector', () => {
       },
     };
 
-    // expect(loadedState).toEqual({});
     expect(promisesSelector(loadedState)).toEqual([combinedPromise]);
+  });
+
+  it('Returns promise with subjects objects', () => {
+    const politician = 2;
+    const testSubjects: Array<Subject> = [
+      {
+        id: 3,
+        created: 'yesterday',
+        description: 'Some descriptive description',
+        modified: 'today',
+        name: 'Lowering tax',
+        number: null,
+        parent: null,
+        parliament_session: 1,
+      },
+      {
+        id: 6,
+        created: 'today',
+        description: 'Another descriptive description',
+        modified: 'tomorrow',
+        name: 'Increase taxes',
+        number: null,
+        parent: null,
+        parliament_session: 1,
+      },
+    ];
+
+    const combinedPromise = {
+      name: 'Some title',
+      small_description: 'Another title',
+      long_description: 'Yet another title',
+      parliament: {
+        name: 'Test-Parliament',
+        id: 1,
+      },
+      id: 4,
+      created: '2017-06-06T19:36:10.404869Z',
+      modified: '2017-06-06T19:36:10.404898Z',
+      politician: politician,
+      party: 1,
+      fulfilled: true,
+      subjects: testSubjects,
+    };
+
+    const subjectsPromises = [
+      {
+        id: 1,
+        promise: 4,
+        subject: 3,
+        created: '2017-11-12T12:24:14.060672Z',
+        modified: '2017-11-12T12:24:14.060699Z',
+      },
+      {
+        id: 2,
+        promise: 4,
+        subject: 6,
+        created: '2017-11-12T12:24:14.060672Z',
+        modified: '2017-11-12T12:24:14.060699Z',
+      },
+    ];
+
+    const loadedState = {
+      ...initialState,
+      politicians: {
+        ...initialState.politicians,
+        politicians,
+      },
+      promises: {
+        ...initialState.promises,
+        promises: [promises[0]],
+      },
+      subjects: {
+        ...initialState.subjects,
+        subjects: testSubjects,
+      },
+      subjectsPromises: {
+        ...initialState.subjectsPromises,
+        subjectsPromises,
+      },
+    };
+
+    deepFreeze(loadedState);
+
+    expect(promiseSubjectsSelector(loadedState)).toEqual([combinedPromise]);
   });
 });
--- src/Stores/Promises/selectors.js
+++ src/Stores/Promises/selectors.js
@@ -1,13 +1,34 @@
 /* @flow */
-import type { State, PoliticianType } from '../../types';
+import type { State, PromiseType } from '../../types';
+import { subjectsSelector } from '../Subjects/selectors';
+import { subjectsPromisesSelector } from '../SubjectsPromises/selectors';
+import { createSelector } from 'reselect';
 
-export const promisesSelector = (state: State) => {
+export const promisesSelector = (state: State): ?Array<PromiseType> => {
   const promises = state.promises.promises;
-  const politicians: ?Array<PoliticianType> = state.politicians.politicians;
 
-  if (!politicians || !promises) return null;
+  if (!promises) return null;
 
   return promises;
 };
 
+export const promiseSubjectsSelector = createSelector(
+  promisesSelector,
+  subjectsSelector,
+  subjectsPromisesSelector,
+  (promises, subjects, subjectsPromises) => {
+    if (!promises) {
+      return null;
+    }
+    return promises.map(promise => {
+      const links = subjectsPromises
+        ? subjectsPromises.filter(sP => sP.promise === promise.id)
+        : [];
+      const subs = links.map(
+        link => (subjects ? subjects.find(s => link.subject === s.id) : [])
+      );
+      return { ...promise, subjects: subs };
+    });
+  }
+);
 export const promisesLoaded = (state: State) => state.promises.promises !== null;
