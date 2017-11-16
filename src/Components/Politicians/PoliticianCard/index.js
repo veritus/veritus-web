23a191e227609786854f0e42c56df88a4ea4090e
Ari Hrannar Björnsson <arihrannar@gmail.com>
2017-11-16 19:44:38 +0100

    moved styling to different file

--- src/Components/Politicians/PoliticianCard/index.js
+++ src/Components/Politicians/PoliticianCard/index.js
@@ -4,6 +4,7 @@ import Paper from 'material-ui/Paper';
 import { Link } from 'react-router-dom';
 import LinearProgress from 'material-ui/LinearProgress';
 import type { PoliticianType } from '../../../types';
+import * as styles from './styles';
 
 export type Props = {
   politician: PoliticianType,
@@ -12,32 +13,21 @@ export type Props = {
 
 const PoliticianCard = (props: Props) => {
   const { politician, progress } = props;
-  const containerStyle = {
-    flex: '0 1 20%',
-    margin: '1%',
-    display: 'flex',
-    flexWrap: 'wrap',
-    alignItems: 'center',
-    textDecoration: 'none',
-  };
+
   if (!politician) return <div />;
   return (
-    <Link style={containerStyle} to={`/politicians/${politician.id}`}>
+    <Link style={styles.container} to={`/politicians/${politician.id}`}>
       <Paper style={{ flex: '1 1 100%', display: 'flex', flexWrap: 'wrap' }}>
         <img
           src="http://lorempixel.com/200/200/"
           alt="Politician"
-          style={{ flex: '1 1 100%' }}
+          style={styles.flexItem}
         />
-        <p style={{ flex: '1 1 100%', textAlign: 'center' }}>{politician.name}</p>
+        <p style={styles.name}>{politician.name}</p>
         <span style={{ marginLeft: `${progress}%` }}>
           {progress}%
         </span>
-        <LinearProgress
-          style={{ flex: '1 1 100%' }}
-          mode="determinate"
-          value={progress}
-        />
+        <LinearProgress style={styles.flexItem} mode="determinate" value={progress} />
       </Paper>
     </Link>
   );
---
+++ src/Components/Politicians/PoliticianCard/styles.js
@@ -0,0 +1,17 @@
+export const container = {
+  flex: '0 1 20%',
+  margin: '1%',
+  display: 'flex',
+  flexWrap: 'wrap',
+  alignItems: 'center',
+  textDecoration: 'none',
+};
+
+export const flexItem = {
+  flex: '1 1 100%',
+};
+
+export const name = {
+  ...flexItem,
+  textAlign: 'center',
+};
