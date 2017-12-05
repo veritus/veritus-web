0a2bec64733e95d0ddb34db1e2bda0a44e3f42d9
Ari Hrannar Björnsson <arihrannar@gmail.com>
2017-12-05 19:26:51 +0100

    adding district to story

--- src/Components/Politicians/PoliticianItem/stories/PoliticianItem.stories.js
+++ src/Components/Politicians/PoliticianItem/stories/PoliticianItem.stories.js
@@ -3,9 +3,13 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { muiTheme } from 'storybook-addon-material-ui';
 import PoliticianItem from '../index';
-import { politicians } from '../../../../utils/testFixtures';
+import { politicians, districts } from '../../../../utils/testFixtures';
 
 storiesOf('Politician Item', module)
   .addDecorator(muiTheme())
-  .add('Andrés Ingi', () => <PoliticianItem politician={politicians[0]} />)
-  .add('Óttarr Proppé', () => <PoliticianItem politician={politicians[1]} />);
+  .add('Andrés Ingi', () =>
+    <PoliticianItem politician={politicians[0]} district={district[0]} />
+  )
+  .add('Óttarr Proppé', () =>
+    <PoliticianItem politician={politicians[1]} district={district[0]} />
+  );
