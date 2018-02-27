e50e28b0ae714bdf000881e1b5b2240456a9e830
Ari Hrannar Björnsson <arihrannar@gmail.com>
2018-02-27 18:32:36 +0100

    Adding possible fields to case detail page

--- src/Components/Cases/CaseDetail/index.js
+++ src/Components/Cases/CaseDetail/index.js
@@ -1,17 +1,20 @@
 /* @flow */
 import React from 'react';
+import moment from 'moment';
+
 import CircularProgress from 'material-ui/CircularProgress';
-import { Card, CardTitle } from 'material-ui/Card';
+import { Card, CardTitle, CardText } from 'material-ui/Card';
 import type { Case } from '../types';
 
 export type Props = {
-  case: ?Case,
+  parliamentCase: ?Case,
 };
 
 export class CaseDetail extends React.Component {
   props: Props;
   render() {
-    if (!this.props.case || !this.props.case.id) {
+    const { parliamentCase } = this.props;
+    if (!parliamentCase || !parliamentCase.id) {
       return (
         <CircularProgress
           style={{ position: 'absolute', top: '50%', left: '50%' }}
@@ -27,7 +30,37 @@ export class CaseDetail extends React.Component {
     };
     return (
       <Card style={style}>
-        <CardTitle title={this.props.case.name} />
+        <CardTitle title={parliamentCase.name} />
+        <CardText>
+          <div>
+            <div>
+              {parliamentCase.case_type}
+              {parliamentCase.status}
+            </div>
+            <div>
+              <a href={parliamentCase.althingi_link}>
+                Althingi
+              </a>
+              {parliamentCase.althingi_status}
+            </div>
+            <div>
+              Creators:
+              {parliamentCase.case_creators}
+            </div>
+            <div>
+              <div>
+                Created:
+                {moment(parliamentCase.created).format('D MMM YYYY')}
+
+              </div>
+              <div>
+                Last Modified:
+                {moment(parliamentCase.modified).format('D MMM YYYY')}
+              </div>
+            </div>
+
+          </div>
+        </CardText>
       </Card>
     );
   }
