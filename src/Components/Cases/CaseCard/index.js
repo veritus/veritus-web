86cc9cd4cc04fc162e1d1109dc9bf78fb8866145
Ari Hrannar Bjornsson <arihrannar@gmail.com>
2018-02-27 17:58:22 +0100

    List of cases facelift and now leads to case detail page (#63)
    * cases page now looks a bit better with flex
    
    * renaming item to case and removing unneeded flex attribute
    
    * added link that leads to case detail page
    
    * mvoing case card styles to its own file

--- src/Components/Cases/CaseCard/index.js
+++ src/Components/Cases/CaseCard/index.js
@@ -1,69 +1,28 @@
 /* @flow */
 import React from 'react';
-import { Card, CardHeader, CardText } from 'material-ui/Card';
+import { Link } from 'react-router-dom';
 import Paper from 'material-ui/Paper';
 
 import type { Case } from '../types';
-
-const moment = require('moment');
+import styles from './styles';
 
 export type Props = {
   parliament_case: Case,
 };
 
-const styles = {
-  container: {
-    width: '100%',
-    overflow: 'auto',
-  },
-  field: {
-    width: '27%',
-    margin: '3%',
-    float: 'left',
-  },
-};
-
 const CaseCard = (props: Props) => {
   const { parliament_case } = props;
   return (
-    <Card key={parliament_case.id}>
-      <CardHeader
-        title={parliament_case.name}
-        subtitle={parliament_case.parliament_session}
-        actAsExpander={true}
-        showExpandableButton={true}
-      />
-      <CardText expandable={true}>
-        <Paper zDepth={1} rounded={false}>
-          <div style={styles.container}>
-            <div style={styles.field}>
-              <span>Parliament: {parliament_case.parliament_session}</span>
-            </div>
-            <div style={styles.field}>
-              <span>Parliament session: {parliament_case.parliament_session}</span>
-            </div>
-            <div style={styles.field}>
-              <span>Type: {parliament_case.case_type}</span>
-            </div>
-          </div>
-          <div style={styles.container}>
-            <div style={styles.field}>
-              <span>Status: {parliament_case.status}</span>
-            </div>
-            <div style={styles.field}>
-              <span>
-                Created on {moment(parliament_case.created).format('MMM Mo YYYY')}
-              </span>
-            </div>
-            <div style={styles.field}>
-              <span>
-                Last Modified on {moment(parliament_case.modified).format('MMM Mo YYYY')}
-              </span>
-            </div>
-          </div>
-        </Paper>
-      </CardText>
-    </Card>
+    <Link to={`/cases/${parliament_case.id}`} style={styles.link}>
+      <Paper zDepth={1} rounded={false} style={styles.container}>
+        <div>
+          <h2>{parliament_case.name}</h2>
+        </div>
+        <div>
+          <span>{parliament_case.case_type}</span>
+        </div>
+      </Paper>
+    </Link>
   );
 };
 
---
+++ src/Components/Cases/CaseCard/styles.js
@@ -0,0 +1,11 @@
+export default {
+  container: {
+    padding: 10,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  link: {
+    textDecoration: 'none',
+  },
+};
