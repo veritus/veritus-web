2b2652b3c81843eb5b0a6d5fd68e8c01216a074e
Ari Hrannar Björnsson <arihrannar@gmail.com>
2018-03-03 16:26:24 +0100

    creating vote action and api files

---
+++ src/Stores/Votes/actions.js
@@ -0,0 +1,33 @@
+/* @flow */
+import type { Dispatch } from '../../types';
+import type { VoteRecord } from './types';
+import type { CaseId } from '../Cases/types';
+
+import actionTypes from './actionTypes';
+
+import { getVoteRecordByCaseId as apiGetVoteRecordByCaseId } from './api';
+
+/* 
+    GET CASE BY ID
+*/
+
+const getVoteRecordByCaseId = () => ({ type: actionTypes.GET_VOTES_BY_CASE_REQUEST });
+
+const getVoteRecordByCaseIdSuccess = (voteRecord: VoteRecord) => ({
+  case: voteRecord,
+  type: actionTypes.GET_VOTES_BY_CASE_SUCCESS,
+});
+
+const getVoteRecordByCaseIdFailure = (error: *) => ({
+  error,
+  type: actionTypes.GET_VOTES_BY_CASE_FAILURE,
+});
+
+export const fetchVoteRecordByCaseId = (id: CaseId) => (dispatch: Dispatch) => {
+  dispatch(getVoteRecordByCaseId());
+
+  return apiGetVoteRecordByCaseId(id).then(
+    resp => resp.data && dispatch(getVoteRecordByCaseIdSuccess(resp.data)),
+    error => dispatch(getVoteRecordByCaseIdFailure(error.message))
+  );
+};
---
+++ src/Stores/Votes/api.js
@@ -0,0 +1,17 @@
+/* @flow */
+
+import type { VoteRecord } from './types';
+import type { CaseId } from '../Cases/types';
+import { serverBaseUrl } from '../../config';
+import { checkStatus, parseJSON, mapData } from '../../utils/api';
+import type { DataResponse } from '../../utils/api';
+
+export const getVoteRecordByCaseId = (caseId: CaseId): DataResponse<VoteRecord> => {
+  return fetch(`${serverBaseUrl}/v1/voteRecord?case=${caseId}`, {
+    accept: 'application/json',
+  })
+    .then(checkStatus)
+    .then(parseJSON)
+    .then(mapData)
+    .catch(error => ({ error }));
+};
