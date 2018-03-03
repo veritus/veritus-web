1e4018500e877c9d8f4fa188f69a8910c5a1964e
Ari Hrannar Björnsson <arihrannar@gmail.com>
2018-03-03 15:57:13 +0100

    Fetch case by id now looks into state to determine if to fetch or not

--- src/Components/Cases/CaseDetailContainer/index.js
+++ src/Components/Cases/CaseDetailContainer/index.js
@@ -1,34 +1,61 @@
 /* @flow */
 import React from 'react';
+import { bindActionCreators } from 'redux';
+import type { ActionCreator } from 'redux';
+import { connect } from 'react-redux';
+import type { Connector } from 'react-redux';
 import CircularProgress from 'material-ui/CircularProgress';
+
 import CaseDetail from '../CaseDetail';
-import { getParliamentCaseById } from '../../../utils/api';
+import { fetchCaseById } from '../../../Stores/Cases/actions';
 import type { MatchTypeCaseId } from '../../../types';
 import type { Case } from '../../../Stores/Cases/types';
+import type { Dispatch, State } from '../../../types';
+import { caseById } from '../../../Stores/Cases/selectors';
 
-export type Props = {
+export type OwnProps = {
   match: MatchTypeCaseId,
 };
 
-export type State = {
+type ReduxProps = {
   parliamentCase: ?Case,
+  hasLoadedCases: boolean,
+  error: ?string,
+  fetchCaseById: ActionCreator<*, *>,
 };
 
-export class CaseDetailContainer extends React.Component<void, Props, State> {
+type Props = OwnProps & ReduxProps;
+
+export class CaseDetailContainer extends React.Component {
   props: Props;
-  state = {
-    parliamentCase: null,
-  };
+
   componentDidMount() {
-    const { match: { params: { caseId } } } = this.props;
-    getParliamentCaseById(caseId).then(resp => {
-      this.setState({ parliamentCase: resp.data });
-    });
+    const { parliamentCase, match: { params: { caseId } } } = this.props;
+    if (!parliamentCase) {
+      this.props.fetchCaseById(caseId);
+    }
   }
   render() {
-    const { parliamentCase } = this.state;
+    const { parliamentCase } = this.props;
     if (!parliamentCase) return <CircularProgress />;
     return <CaseDetail parliamentCase={parliamentCase} />;
   }
 }
-export default CaseDetailContainer;
+
+const mapStateToProps = (state: State, ownProps: OwnProps) => {
+  return {
+    error: state.cases.error,
+    hasLoadedCases: state.cases.loading,
+    parliamentCase: caseById(state, ownProps.match.params.caseId),
+  };
+};
+
+const mapDispatchToProps = (dispatch: Dispatch) =>
+  bindActionCreators({ fetchCaseById }, dispatch);
+
+const connector: Connector<OwnProps, Props> = connect(
+  mapStateToProps,
+  mapDispatchToProps
+);
+
+export default connector(CaseDetailContainer);
